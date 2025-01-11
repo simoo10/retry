dc2364b13e60f4c7512f418721952d17775458b6
Mohamed Et-Tahery <met-tahe@e1r8p1.1337.ma>
2025-01-11 21:00:41 +0100

    SPA Retry

---
+++ frontend/js/main.js
@@ -0,0 +1,1 @@
+//function dyal pages routes;
---
+++ frontend/js/theme.js
@@ -0,0 +1,7 @@
+
+function toggleTheme() {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+}

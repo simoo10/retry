e2f682e621249ab3af0d4660c75f8e2172f5072e
Mohamed Et-Tahery <met-tahe@e1r6p1.1337.ma>
2025-01-19 18:45:19 +0100

    SPA Retry

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -164,7 +164,7 @@ AUTH_USER_MODEL = 'api.Intra42User'
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1), 
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,
--- frontend/js/rendringData.js
+++ frontend/js/rendringData.js
@@ -5,7 +5,43 @@ function getCookie(name) {
     if (parts.length === 2) return parts.pop().split(";").shift();
     return null;
   }
+function render_profile(data){
+    const jsonString = data.image.replace(/'/g, '"');
+    const imageData = JSON.parse(jsonString);
+    console.log(imageData);
+    const profile_img = imageData.link;
+    const profile = document.getElementsByClassName('profile')[0];
+    profile.innerHTML = `
+            <div class="usr">
+            <img src="${profile_img}" alt="profile" class="profile-img">
+                <div class=user-info>
+                    <span class="username">${data.login}</span>
+                    <span class="rank">Rank</span>
+                </div>
+            </div>
+                    `;
+    const user_info = document.getElementsByClassName('user-info')[0];
+    const profile_img_style = document.getElementsByClassName('profile-img')[0];
+    const username_style = document.getElementsByClassName('username')[0];
+    const usr_style = document.getElementsByClassName('usr')[0];
+    profile.style.display = 'flex';
+    profile.style.alignItems = 'center';
+    profile.style.marginBottom = '20px';
+    usr_style.style.display = 'flex';
+    usr_style.style.alignItems = 'flex-start';
+    usr_style.style.flexDirection = 'row';
+    profile_img_style.style.borderRadius = '50%';
+    profile_img_style.style.width = '100px';
+    profile_img_style.style.height = '100px';
+    profile_img_style.style.marginRight = '15px';
+    user_info.style.display = 'flex';
+    user_info.style.flexDirection = 'column';
+    user_info.style.marginTop = '20px';
+    username_style.style.alignItems = 'flex-start';
 
+    
+
+}
 export async function fetching_data(){
     const access_token = getCookie('access_token');
     try{
@@ -18,19 +54,8 @@ export async function fetching_data(){
         });
         if(response.ok){
             const data = await response.json();
-            // console.log("-----=+>",data1.image);
-            console.log(Object.keys(data.image)); 
-            // sleep(7000);
-            const profile = document.getElementsByClassName('profile')[0];
-            profile.innerHTML = `
-           
-            <div class="usr">
-            <img src="${data.image['link']}" alt="profile">
-                <span class="username">${data.login}</span>
-                <span class="rank">Rank</span>
-            </div>
-                    `;
-                    return;
+            render_profile(data);
+            return;
         }
         else{
             console.error('Failed to fetch data');
